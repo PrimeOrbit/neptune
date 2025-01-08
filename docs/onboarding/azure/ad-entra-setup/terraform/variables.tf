146031d18bc12355d06471236123a4993d3d7a2c
Mahadev Alladi <alladi@primeorbit.com>
2025-01-08 14:50:42 -0800

    Adding scripts for Azure deployment

---
+++ docs/onboarding/azure/ad-entra-setup/terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "include_special_charecter_in_secret" {
+    description = "(optional) -> Include Special charecters in Secret (default:false)"
+    default = false
+}
+
+variable "redirect_uri" {
+  description = "Please enter (required) -> AD Redirect URI (Eg: https://costapp.example.com/)"
+}
